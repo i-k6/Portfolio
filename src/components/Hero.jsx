fc3d9fa53bc4791c53857ba1494149ffbfdca8fa
Karan <46293088+i-k6@users.noreply.github.com>
2023-12-20 12:38:11 +0530

    added cv download button and few more changes in the project card

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { styles } from "../styles";
 import Typed from "typed.js";
-
+import Resume from "../assets/Resume_Cv/Resume.pdf.pdf"
 const Hero = () => {
   const typedRef = React.useRef(null);
+  const [clicked, setClicked] = useState(false);
 
-  React.useEffect(() => {
+  const handleClick = () => {
+    const link = document.createElement('a');
+    link.href = Resume;
+    link.download = "Karan's_Resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 230); 
+  };
+
+  useEffect(() => {
     const options = {
       strings: ["Hi, I'm <span class='text-[#915eff]'>Karan</span>"],
-      typeSpeed: 100, 
-      showCursor: false, 
+      typeSpeed: 100,
+      showCursor: false,
       contentType: 'html',
     };
     typedRef.current = new Typed("#typed-text", options);
@@ -37,15 +52,21 @@ const Hero = () => {
             className={`${styles.heroHeadText} text-white`}
           ></h1>
           <p className={`${styles.heroSubText} mt-3 text-white-100`}>
-            I'm a dedicated web developer
+            I'm a web developer
             <br className="sm:block hidden" /> with a passion for crafting
             interactive and user-friendly websites.
           </p>
+          <button
+            className={`bg-[#915eff] text-white px-6 py-3 mt-4 rounded-md transition duration-300 hover:bg-opacity-80 ${clicked ? 'transform scale-95' : ''}`}
+            style={{ borderRadius: "12px" }}
+            onClick={handleClick}
+          >
+            Download CV
+          </button>
         </div>
       </div>
     </section>
   );
 };
-
 
 export default Hero;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { github, link } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import './Works.css'
 
 const ProjectCard = ({
   index,
@@ -17,7 +17,7 @@ const ProjectCard = ({
   live_link
 }) => {
   return (
-    <motion.div  variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
@@ -32,37 +32,11 @@ const ProjectCard = ({
             alt={name}
             className="w-full h-full rounded-2xl object-cover"
           />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={github}
-                href={source_code_link}
-                alt="source code"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-            <div
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              onClick={() => window.open(live_link, "_blank")}
-            >
-              <img
-                href={live_link}
-                src={link}
-                alt="Live link"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-          </div>
         </div>
-
         <div className="mt-5">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
-
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
@@ -72,6 +46,24 @@ const ProjectCard = ({
               #{tag.name}
             </p>
           ))}
+        </div>
+        <div className=" mt-4 flex gap-4">
+          <a
+            href={source_code_link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-secondary text-white  button px-4 py-2 rounded-md transition duration-300 hover:bg-secondary-dark"
+          >
+            GitHub
+          </a>
+          <a
+            href={live_link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-primary text-white  button px-4 py-2 rounded-md transition duration-300 hover:bg-primary-dark"
+          >
+            Live
+          </a>
         </div>
       </Tilt>
     </motion.div>
